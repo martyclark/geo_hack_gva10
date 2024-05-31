64934ce5309969326d5d12078fb422e3f1dfa179
burnout87 <gabriele.barni@gmail.com>
2024-05-31 20:56:02 +0200

    dynamic update

--- data_helper.py
+++ data_helper.py
@@ -2,9 +2,11 @@ import os
 import json
 
 data_path = os.path.join(os.path.dirname(__file__), 'data')
+city_heat_maps_path = os.path.join(os.path.dirname(__file__), 'data', 'city_heat_maps')
 global_data_file = os.path.join(data_path, 'global_dataset.geojson')
 
-def load_data_and_labels():
+
+def load_global_data_and_labels():
     """
     Load data and labels from the geojson file
     """
@@ -14,6 +16,18 @@ def load_data_and_labels():
         data = json.load(f)
 
     return data['features']
+
+
+def get_heat_map_by_city_name(city_name):
+    file_path = os.path.join(city_heat_maps_path, city_name + '.geojson')
+    # Load data from files
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            data = json.load(f)
+
+        return data['features']
+    else:
+        return None
 
 
 def get_data_by_id(data, facility_id):
--- description.py
+++ description.py
@@ -4,7 +4,7 @@ from folium import Popup
 from streamlit_folium import st_folium
 import data_helper
 
-data = data_helper.load_data_and_labels()
+data = data_helper.load_global_data_and_labels()
 
 # Set page layout to wide
 st.set_page_config(layout="wide")
@@ -14,8 +14,13 @@ st.write("# Welcome to our Project! 👋")
 # Create two columns with the first one being twice as wide as the second
 col1, col2 = st.columns([2, 1])
 
-# Center on Liberty Bell and add markers
-m = folium.Map(location=[0, 0], zoom_start=5)
+# Initialize city_map in session_state if it doesn't exist
+# if 'city_map' not in st.session_state:
+#     print("not in session state")
+#     st.session_state.city_map = folium.Map(location=[0, 0], zoom_start=5)
+
+global_map = folium.Map(location=[0, 0], zoom_start=5)
+btn_expand = st.button("display in second map")
 
 for f in data:
     facility_id = str(f["properties"]["ID_HDC_G0"])
@@ -24,20 +29,46 @@ for f in data:
         popup=Popup(facility_id, parse_html=False),
         tooltip="Tooltip!"
     )
-    marker.add_to(m)
+    marker.add_to(global_map)
 
 # Add a button to the right column
 with col1:
     # Render Folium map in Streamlit
-    out = st_folium(m, width=900, height=400)
+    global_map_obj = st_folium(global_map, width=900, height=400)
 
 
 # Display the popup and tooltip information
 with col2:
-    properties = data_helper.get_data_by_id(data, out["last_object_clicked_popup"])
+    properties = data_helper.get_data_by_id(data, global_map_obj["last_object_clicked_popup"])
     if properties is not None:
         st.write("Info object:", properties)
+        city_data = data_helper.get_heat_map_by_city_name(properties["UC_NM_MN"].lower())
 
-if out["last_object_clicked_popup"]:
-    st.button("Show More Data")
-    
+
+# with st.form(key="form_display_city_map"):
+# city_map = folium.Map(location=[0, 0], zoom_start=5)
+if btn_expand:
+    # marker = folium.Marker(
+    #     location=[5.621085071, -0.215586875],
+    #     popup=Popup("test", parse_html=False),
+    #     tooltip="Tooltip!"
+    # )
+    # marker.add_to(st.session_state.city_map)
+    #
+    # city_map_obj = st_folium(st.session_state.city_map, width=900, height=400)
+    # # st.session_state.city_map.render()
+    m = folium.Map(location=[5.621085071, -0.215586875], zoom_start=8)
+    fg = folium.FeatureGroup(name="Markers")
+    for marker in st.session_state["markers"]:
+        fg.add_child(marker)
+
+    st_folium(
+        m,
+        center=st.session_state["center"],
+        zoom=st.session_state["zoom"],
+        key="new",
+        feature_group_to_add=fg,
+        height=400,
+        width=700,
+    )
+
