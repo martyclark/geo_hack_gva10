3c804a6a8a4422115774e07ae655ce9489aeb34a
VimedGit <vimedpo@gmail.com>
2024-05-31 15:19:16 +0200

    including folium map

--- Description.py
+++ Description.py
@@ -1,3 +1,4 @@
+import folium
 import streamlit as st
 
 import data_helper
@@ -19,3 +20,35 @@ col2.markdown(
     Placeholder text for overall info / info about specific country
     """
 )
+
+
+from streamlit_folium import st_folium
+from folium import Popup
+
+# center on Liberty Bell, add marker
+m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
+folium.Marker(
+    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
+).add_to(m)
+
+# call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
+
+m = folium.Map(location=[45, -122], zoom_start=4)
+
+folium.Marker(
+    location=[45.5, -122],
+    popup=Popup("Popup!", parse_html=False),
+    tooltip="Tooltip!",
+).add_to(m)
+
+folium.Marker(
+    location=[45.5, -112],
+    popup=Popup("Popup 2!", parse_html=False),
+    tooltip="Tooltip 2!",
+).add_to(m)
+
+out = st_folium(m, height=200)
+
+st.write("Popup:", out["last_object_clicked_popup"])
+st.write("Tooltip:", out["last_object_clicked_tooltip"])
