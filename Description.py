3c4fd067c10f31c19c28d7d4e7395d2814f33b99
burnout87 <gabriele.barni@gmail.com>
2024-05-31 18:57:14 +0200

    read data from the file when clicking on map

--- Description.py
+++ Description.py
@@ -18,11 +18,11 @@ col1, col2 = st.columns([2, 1])
 m = folium.Map(location=[0, 0], zoom_start=5)
 
 for f in data:
+    facility_id = str(f["properties"]["ID_HDC_G0"])
     marker = folium.Marker(
         location=f['geometry']['coordinates'][::-1],
-        popup=Popup("Popup!", parse_html=False),
-        tooltip="Tooltip!",
-        kwargs=f
+        popup=Popup(facility_id, parse_html=False),
+        tooltip="Tooltip!"
     )
     marker.add_to(m)
 
@@ -30,12 +30,13 @@ for f in data:
 with col1:
     # Render Folium map in Streamlit
     out = st_folium(m, width=700, height=400)
-    print(out)
+
 
 # Display the popup and tooltip information
 with col2:
-    st.write("Popup:", out["last_object_clicked_popup"])
-    st.write("Tooltip:", out["last_object_clicked_tooltip"])
+    properties = data_helper.get_data_by_id(data, out["last_object_clicked_popup"])
+    if properties is not None:
+        st.write("Info object:", properties)
 
 if out["last_object_clicked_popup"]:
-        st.button("Show More Data")
+    st.button("Show More Data")
--- data_helper.py
+++ data_helper.py
@@ -2,7 +2,7 @@ import os
 import json
 
 data_path = os.path.join(os.path.dirname(__file__), 'data')
-deta_file = os.path.join(data_path, 'global_dataset.geojson')
+global_data_file = os.path.join(data_path, 'global_dataset.geojson')
 
 def load_data_and_labels():
     """
@@ -10,7 +10,14 @@ def load_data_and_labels():
     """
 
     # Load data from files
-    with open(deta_file, 'r') as f:
+    with open(global_data_file, 'r') as f:
         data = json.load(f)
 
     return data['features']
+
+
+def get_data_by_id(data, facility_id):
+    for f in data:
+        if str(f["properties"]["ID_HDC_G0"]) == facility_id:
+            return f["properties"]
+    return None
