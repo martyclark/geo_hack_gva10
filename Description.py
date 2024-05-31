3e3714afcf99492db4fb1d1640ebdb1eeefdec44
chewyongzhang <88772274+chewyongzhang@users.noreply.github.com>
2024-05-31 17:52:26 +0200

    implementing interactive map

--- Description.py
+++ Description.py
@@ -15,16 +15,6 @@ st.write("# Welcome to our Project! 👋")
 # Create two columns with the first one being twice as wide as the second
 col1, col2 = st.columns([2,1])
 
-# Add a map to the first column
-col1.map()
-
-# Add text to the second column
-col2.markdown(
-    """
-    Placeholder text for overall info / info about specific country
-    """
-)
-
 # center on Liberty Bell, add marker
 m = folium.Map(location=[0, 0], zoom_start=5)
 
@@ -36,23 +26,12 @@ for f in data:
     ).add_to(m)
 
 # call to render Folium map in Streamlit
-st_data = st_folium(m, width=725)
+# st_data = st_folium(m, width=725)
 
-m = folium.Map(location=[45, -122], zoom_start=4)
+with col1:
+    out = st_folium(m, width=700)
+with col2:
+    st.write("Popup:", out["last_object_clicked_popup"])
+    st.write("Tooltip:", out["last_object_clicked_tooltip"])
 
-folium.Marker(
-    location=[45.5, -122],
-    popup=Popup("Popup!", parse_html=False),
-    tooltip="Tooltip!",
-).add_to(m)
 
-folium.Marker(
-    location=[45.5, -112],
-    popup=Popup("Popup 2!", parse_html=False),
-    tooltip="Tooltip 2!",
-).add_to(m)
-
-out = st_folium(m, height=200)
-
-st.write("Popup:", out["last_object_clicked_popup"])
-st.write("Tooltip:", out["last_object_clicked_tooltip"])
