ae8df3c8c7c9e5508575bee59c22ffb067df8ee9
burnout87 <gabriele.barni@gmail.com>
2024-06-01 11:22:09 +0200

    more code clean-up

--- description.py
+++ description.py
@@ -6,12 +6,8 @@ import data_helper
 
 data = data_helper.load_global_data_and_labels()
 
-# Set page layout to wide
 st.set_page_config(layout="wide")
 
-st.write("# Welcome to our Project! 👋")
-
-# Create two columns with the first one being twice as wide as the second
 col1, col2 = st.columns([2, 1])
 
 if "zoom" not in st.session_state:
