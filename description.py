fa7761d62492a020210d91451f71f612feaa3c1f
martyclark <martyclark@users.noreply.github.com>
2024-06-11 17:32:42 +0200

    Update description.py

--- description.py
+++ description.py
@@ -7,22 +7,25 @@ from streamlit_folium import st_folium
 import data_helper
 import random
 
+# Load data
 data = data_helper.load_global_data_and_labels()
 
+# Constants
 MAP_EMOJI_URL = "./Logo.png"
 
+# Streamlit page configuration
 st.set_page_config(
     page_title="Global Heat Resilience Service", 
     page_icon=MAP_EMOJI_URL,
-    layout="wide")
+    layout="wide"
+)
 
-
+# Utility functions
 def get_random_color():
     r = random.randrange(0, 256)
     g = random.randrange(0, 256)
     b = random.randrange(0, 256)
-    return '#{:02x}{:02x}{:02x}'.format(r,g,b)
-
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
 def create_style_function(fill_color, border_color=None):
     def style_function(feature):
@@ -34,20 +37,18 @@ def create_style_function(fill_color, border_color=None):
         }
     return style_function
 
-
-col6, col7,col8 = st.columns([1,5,1])
-col6.write("")
-with col7:
-    st.image(MAP_EMOJI_URL, width=160)
-    st.title("Global Heat Resilience Service")
-    st.write('''Cities worldwide face the critical challenge of understanding and addressing localized extreme heat risks, a top climate change concern. The Global Heat Resilience Service is designed to empower urban decision-makers with the comprehensive data needed to prioritize and tackle this issue effectively. Our platform seamlessly integrates existing open global data, providing detailed neighborhood-level insights into heat vulnerability. It captures key factors such as demographics, socio-economic status, and access to essential services, which traditional methods often overlook.   
-          
-With our user-friendly interface, information that was once inaccessible and fragmented is now consolidated and easily navigable. The Global Heat Resilience Service offers a robust combination of hazard, exposure, and vulnerability data, enabling users to assess heat vulnerability under both current and future climatic conditions. Our clear maps and data visualizations highlight the location and number of people exposed and vulnerable, detailing impacts on health, economic losses, and more. By presenting these insights in an accessible format, the Global Heat Resilience Service supports cities in making informed decisions and implementing effective strategies to protect their communities from the growing threat of extreme heat. Join us in building more resilient urban environments and safeguarding the well-being of residents worldwide.''')
-col8.write("")
+# Header section
+st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
+st.image(MAP_EMOJI_URL, width=160)
+st.title("Global Heat Resilience Service")
+st.markdown('''
+Cities worldwide face the critical challenge of understanding and addressing localized extreme heat risks...
+''')
+st.markdown("</div>", unsafe_allow_html=True)
 st.divider()
 
-
-col1, col2 = st.columns([2, 1])
+# Main content
+col1, col2 = st.columns([3, 2])
 
 if "zoom" not in st.session_state:
     st.session_state["zoom"] = 5
@@ -58,16 +59,14 @@ if "markers_2030" not in st.session_state:
 if "markers_2050" not in st.session_state:
     st.session_state["markers_2050"] = []
 
-#add global basemap, with stamen styling
-
+# Create global map
 global_map = folium.Map(location=[0, 0], zoom_start=5)
-
-# Add a 'Stamen Toner' TileLayer to the map
 folium.TileLayer(
     tiles='Stamen Toner',  
     attr='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> | Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 ).add_to(global_map)
 
+# Add markers to global map
 for f in data:
     facility_id = str(f["properties"]["ID_HDC_G0"])
     marker = folium.Marker(
@@ -77,29 +76,28 @@ for f in data:
     )
     marker.add_to(global_map)
 
+# Display global map
 with col1:
+    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
     global_map_obj = st_folium(global_map, width=900, height=400)
+    st.markdown("</div>", unsafe_allow_html=True)
+
+    # Create city-specific map
     city_map = folium.Map(location=[0, 0], zoom_start=8, min_zoom=3, max_zoom=10)
     fg_2020 = folium.FeatureGroup(name="2020", overlay=True, show=True)
-    # fg_2030 = folium.FeatureGroup(name="2030", overlay=True, show=False)
-    # fg_2050 = folium.FeatureGroup(name="2050", overlay=True, show=False)
-    # fg = folium.FeatureGroup(name="Markers")
+
+    # Add feature group to city map
     for marker in st.session_state["markers_2020"]:
         fg_2020.add_child(marker)
-    # for marker in st.session_state["markers_2030"]:
-    #     fg_2030.add_child(marker)
-    # for marker in st.session_state["markers_2050"]:
-    #     fg_2050.add_child(marker)
-    # folium.TileLayer('openstreetmap', overlay=False, name="years").add_to(city_map)
-    # folium.LayerControl().add_to(city_map)
 
- # Update zoom level and center location when a marker is clicked
+    # Update city map based on global map interaction
     if global_map_obj["last_object_clicked_popup"] is not None:
         clicked_marker = data_helper.get_data_by_id(data, global_map_obj["last_object_clicked_popup"])
         if clicked_marker is not None:
             location = [clicked_marker['GCPNT_LAT'], clicked_marker['GCPNT_LON']]
             city_map.fit_bounds([location, location], max_zoom=10)
 
+    st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
     city_map_obj = st_folium(
         city_map,
         key="city_map",
@@ -107,30 +105,25 @@ with col1:
         height=400,
         width=900,
     )
+    st.markdown("</div>", unsafe_allow_html=True)
 
-
-with (col2):
-    col3, col4, col5 = st.columns(3)
+# Display city information
+with col2:
     properties = data_helper.get_data_by_id(data, global_map_obj["last_object_clicked_popup"])
-    heat_map_properties = data_helper.get_data_by_id(data, city_map_obj["last_object_clicked_popup"])
+
     if properties is not None:
         st.session_state["markers_2020"] = []
-        with col3:
-            st.metric("Country name", properties["CTR_MN_NM"])
-            st.write("Total area of Urban Centres in 2000:")
-            st.subheader(properties["H00_AREA"])
-        with col4:
-            st.metric("City name", properties["UC_NM_MN"])
-            st.write("Total built-up area in 2015:")
-            st.subheader(round(properties["B15"],0))
-        with col5:
-            st.metric ("Area", properties["AREA"]) 
-            st.write("Average temperature for epoch 2014:")
-            st.subheader(round(properties["E_WR_T_14"], 1))
-        st.write("Total resident population in 2015:", properties["P15"])
-        st.write("Sum of GDP PPP values for year 2015:", properties["GDP15_SM"])
-        st.write("Average greenness estimated for 2014 located in the built-up area of epoch 2014:", properties["E_GR_AV14"])
-        st.write("Maximum magnitude of the heatwaves", properties["EX_HW_IDX"])
+        st.markdown("### City Information")
+        st.markdown("<p style='font-size: 14px;'><b>Country Name:</b> {}</p>".format(properties["CTR_MN_NM"]), unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 14px;'><b>Total Area of Urban Centres in 2000:</b> {} sq km</p>".format(properties["H00_AREA"]), unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 14px;'><b>City Name:</b> {}</p>".format(properties["UC_NM_MN"]), unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 14px;'><b>Total Built-up Area in 2015:</b> {} sq km</p>".format(round(properties["B15"], 0)), unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 14px;'><b>Area:</b> {} sq km</p>".format(properties["AREA"]), unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 14px;'><b>Average Temperature for Epoch 2014:</b> {} °C</p>".format(round(properties["E_WR_T_14"], 1)), unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 14px;'><b>Total Resident Population in 2015:</b> {}</p>".format(properties["P15"]), unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 14px;'><b>Sum of GDP PPP Values for Year 2015:</b> {}</p>".format(properties["GDP15_SM"]), unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 14px;'><b>Average Greenness in 2014:</b> {}</p>".format(properties["E_GR_AV14"]), unsafe_allow_html=True)
+        st.markdown("<p style='font-size: 14px;'><b>Maximum Magnitude of the Heatwaves:</b> {}</p>".format(properties["EX_HW_IDX"]), unsafe_allow_html=True)
 
         city_data = data_helper.get_heat_map_by_city_name(properties["UC_NM_MN"].lower())
         if city_data is not None:
@@ -150,127 +143,78 @@ with (col2):
                     f,
                     popup=folium.features.GeoJsonPopup(fields=["Name", "_median", "_median_2", "_median_3"],
                                                        aliases=[
-                                                       '<p style="font-size:12px;">Name</p>',
-                                                       '<p style="font-size:9px;">Year 2020</p>',
-                                                       '<p style="font-size:9px;">Year 2030</p>',
-                                                       '<p style="font-size:9px;">Year 2050</p>'],
+                                                           'Name',
+                                                           'Year 2020',
+                                                           'Year 2030',
+                                                           'Year 2050'],
                                                        labels=True),
                     style_function=style_function
                 )
                 marker.add_to(city_map)
                 st.session_state["markers_2020"].append(marker)
-                # # 2030
-                # if "colRange_2030" in f["properties"]:
-                #     if f["properties"]["colRange_2030"] is not None:
-                #         fillColorProperties = f["properties"]["colRange_2030"]
-                #         color = "#000000"
-                #     else:
-                #         fillColorProperties = None
-                #         color = 'rgba(0, 0, 0, 0)'
-                # else:
-                #     color = get_random_color()
-                # style_function = create_style_function(fill_color=fillColorProperties, border_color=color)
-                # marker = folium.GeoJson(
-                #     f,
-                #     popup=folium.features.GeoJsonPopup(fields=["_median", "_median_2", "_median_3"]),
-                #     style_function=style_function
-                # )
-                # marker.add_to(city_map)
-                # st.session_state["markers_2030"].append(marker)
-                # # 2050
-                # if "colRange_2050" in f["properties"]:
-                #     if f["properties"]["colRange_2050"] is not None:
-                #         fillColorProperties = f["properties"]["colRange_2050"]
-                #         color = "#000000"
-                #     else:
-                #         fillColorProperties = None
-                #         color = 'rgba(0, 0, 0, 0)'
-                # else:
-                #     color = get_random_color()
-                # style_function = create_style_function(fill_color=fillColorProperties, border_color=color)
-                # marker = folium.GeoJson(
-                #     f,
-                #     popup=folium.features.GeoJsonPopup(fields=["_median", "_median_2", "_median_3"]),
-                #     style_function=style_function
-                # )
-                # marker.add_to(city_map)
-                # st.session_state["markers_2050"].append(marker)
+
         properties = city_map_obj["last_object_clicked_popup"]
         st.write(properties)
+
+# Download button
 with open(os.path.join("data", "Rome Urban Heat Resilience Profile.pdf"), "rb") as file:
-    btn = st.download_button(label ="Download data", data = file, file_name = "Rome Urban Heat Resilience Profile.pdf", mime = "application/pdf")
+    btn = st.download_button(label="Download data", data=file, file_name="Rome Urban Heat Resilience Profile.pdf", mime="application/pdf")
 
 st.divider()
 
-
-with st.expander("Browse avaliable data"):
+# Data browser section
+with st.expander("Browse available data"):
     components.iframe('https://geo-dev-hub.maps.arcgis.com/apps/mapviewer/index.html?webmap=4c17c5dd3cba457e80e6c546bc73a3b1', height=500)
-    
+
 st.divider()
 
-col11, col12, col13 = st.columns([1,5,1])
+# Methodology section
+st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
+st.header("Methodology")
+st.markdown('''
+The heat map visually represents the distribution of extreme heat within the city, highlighting areas with varying temperature intensities...
+''')
+st.markdown("</div>", unsafe_allow_html=True)
+st.divider()
 
-with col12:
-    st.header("Methodology")
-    st.write(''' The heat map visually represents the distribution of extreme heat within the city, highlighting areas with varying temperature intensities. Warmer colors, such as red and orange, indicate higher temperatures and greater heat risk, while cooler colors, such as blue and green, represent lower temperatures and lesser risk.''')
-    st.markdown(''' ## How to Interpret the Heat Map
-                
-### High-Risk Areas  
-                
-Look for regions shaded in red or dark orange, which signify zones with the highest temperatures and potential heat-related impacts.  
-### Moderate-Risk Areas  
-                
-Yellow and light orange areas indicate moderate temperatures and a need for some heat mitigation measures.  
-                
-### Low-Risk Areas  
-                
-Green and blue regions denote cooler areas with lower heat risk.''')
-    st.divider()
+# Historical data section
+st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
+st.header("Historical Data")
+st.markdown('''
+The Historical Temperature Data section provides an in-depth analysis of past temperature trends within the city...
+''')
+st.markdown("</div>", unsafe_allow_html=True)
+st.divider()
 
-    st.header("Historical data")
-    st.write('''The Historical Temperature Data section provides an in-depth analysis of past temperature trends within the city. This section includes comprehensive data on daily, monthly, and annual temperature variations over the past several decades. By examining historical temperature records, users can identify long-term trends, such as increasing average temperatures, the frequency and intensity of heatwaves, and seasonal patterns.''')  
-    st.subheader("Key Features")
-    st.write('''Temperature Trends: Visual graphs and charts showing how temperatures have changed over time.
-Heatwave Analysis: Data on the occurrence, duration, and severity of past heatwaves.
-Seasonal Patterns: Insights into temperature fluctuations across different seasons, highlighting periods of extreme heat.
-Comparison with Averages: Comparisons of historical temperatures with long-term climate averages to identify anomalies and significant changes.''')
-
-    st.subheader("How to Use This Section")
-
-    st.write('''Users can leverage the historical temperature data to:
-
-Understand Long-Term Changes: Gain insights into how the city's climate has evolved, helping to predict future trends.
-Inform Planning and Policy: Use historical data to support urban planning decisions and the development of heat mitigation strategies.
-Raise Awareness: Educate the community and stakeholders about historical temperature patterns and their implications for future heat resilience.''')
-    st.divider()
-st.markdown(''' © 2024 Global Heat Resilience Service. All Rights Reserved. For more information, please contact us at:
-
+# Footer
+st.markdown("<div style='text-align: center;'>", unsafe_allow_html=True)
+st.markdown('''
+© 2024 Global Heat Resilience Service. All Rights Reserved. For more information, please contact us at:
 Email: heatresilience@geosec.org 
 Phone: +41 22 730 8251
-
-Visit our main website: Earth Observations''')
-
-col9, col10 = st.columns([1,1])
-
-with col9:
-    st.write('''
-Useful Links:
-
-    Privacy Policy
-    Terms of Service
-    Help & Support
-    About Us
-    Data Sources
-    Partners
+Visit our main website: Earth Observations
 ''')
-    
-with col10:
-    st.write('''
-Follow Us:
 
-    Facebook
-    Twitter
-    LinkedIn 
-''')
+col1, col2 = st.columns([1, 1])
+
+with col1:
+    st.markdown('''
+    ### Useful Links:
+    - Privacy Policy
+    - Terms of Service
+    - Help & Support
+    - About Us
+    - Data Sources
+    - Partners
+    ''')
+
+with col2:
+    st.markdown('''
+    ### Follow Us:
+    - Facebook
+    - Twitter
+    - LinkedIn 
+    ''')
 
 st.markdown("Global Heat Resilience Service is part of the Group on Earth Observations (GEO) community, dedicated to fostering global collaboration for a sustainable planet.")
+st.markdown("</div>", unsafe_allow_html=True)
