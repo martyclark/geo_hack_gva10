2010eee0472b69c9e9ce42d87344fa9d3c512644
martyclark <martyclark@users.noreply.github.com>
2024-06-12 12:14:42 +0200

    Update description.py

--- description.py
+++ description.py
@@ -134,17 +134,10 @@ with st.container():
         city_map_obj = st_folium(city_map, key="city_map", width=800, height=400)
 
     with col7:
-        if "last_object_clicked_popup" in st.session_state:
-            properties = st.session_state["last_object_clicked_popup"]
-            if properties:
-                st.subheader("District-level heat risk assessment")
-                formatted_properties = f"""
-                **Name:** {properties.get('Name', 'N/A')}  
-                **Year 2020:** {properties.get('_median', 'N/A')}  
-                **Year 2030:** {properties.get('_median_2', 'N/A')}  
-                **Year 2050:** {properties.get('_median_3', 'N/A')}
-                """
-                st.markdown(formatted_properties)
+        properties = city_map_obj.get("last_object_clicked_popup")
+        if properties:
+            st.subheader("District-level heat risk assessment")
+            st.write(properties)
         
 # Download button
 with open(os.path.join("data", "Rome Urban Heat Resilience Profile.pdf"), "rb") as file:
